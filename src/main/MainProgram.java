5b9e0f8f9f73be23665ee57669db38b2e96829da
9hax <it.fritz@gmx.de>
2020-02-19 22:10:37 +0100

    remove unused variable

--- src/main/MainProgram.java
+++ src/main/MainProgram.java
@@ -12,7 +12,7 @@ public class MainProgram {
     Connection connection;
     
     public static void main(String[] args){
-        MainProgram p= new MainProgram();
+        new MainProgram();
     }
     
     public MainProgram(){
